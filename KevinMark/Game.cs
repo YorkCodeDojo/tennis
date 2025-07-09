44acd1e7e238985bee733715b053beb6dbcdddc6
Kevin Smith <kev_bite@msn.com>
2025-07-09 20:12:15 +0100

    Basic Game with Tests.

---
+++ KevinMark/Game.cs
@@ -0,0 +1,73 @@
+﻿namespace KevinMark;
+
+public class Game
+{
+    private Score _playerOneScore;
+    private Score _playerTwoScore;
+
+    public bool AddPlayerOneScore()
+    {
+        (var isValid, _playerOneScore, _playerTwoScore) = AddScore(_playerOneScore, _playerTwoScore);
+
+        return isValid;
+    }
+
+    public bool AddPlayerTwoScore()
+    {
+        (var isValid, _playerTwoScore, _playerOneScore) = AddScore(_playerTwoScore, _playerOneScore);
+
+        return isValid;
+    }
+
+    private string GetStringForScore(Score playerScore)
+    {
+        return playerScore switch
+        {
+            Score.Love => "love",
+            Score.Fifteen => "15",
+            Score.Thirty => "30",
+            Score.Forty => "40",
+            _ => throw new ArgumentOutOfRangeException(nameof(_playerOneScore), _playerOneScore, null)
+        };
+    }
+
+
+    private (bool valid, Score currentPlayer, Score otherPlayer) AddScore(Score currentPlayer, Score otherPlayer)
+    {
+        if (_playerOneScore == Score.Game || _playerTwoScore == Score.Game)
+        {
+            return (false, currentPlayer, otherPlayer);
+        }
+
+        if (currentPlayer == Score.Forty)
+        {
+            if (otherPlayer < Score.Forty)
+                return (true, Score.Game, otherPlayer);
+
+            if (otherPlayer == Score.Forty)
+                return (true, Score.Advantage, otherPlayer);
+
+            if (otherPlayer == Score.Advantage)
+                return (true, currentPlayer, --otherPlayer);
+        }
+
+        return (true, ++currentPlayer, otherPlayer);
+    }
+
+
+    public string GetStringScore()
+    {
+        var result = (_playerOneScore, _playerTwoScore) switch
+        {
+            (Score.Game, _) => "game-player-one",
+            (_, Score.Game) => "game-player-two",
+            (Score.Advantage, _) => "advantage-player-one",
+            (_, Score.Advantage) => "advantage-player-two",
+            (Score.Love, Score.Love) => "love-all",
+            (Score.Forty, Score.Forty) => "deuce",
+            _ => $"{GetStringForScore(_playerOneScore)}-{GetStringForScore(_playerTwoScore)}"
+        };
+
+        return result;
+    }
+}
---
+++ KevinMark/GameTests.cs
@@ -0,0 +1,111 @@
+﻿
+namespace KevinMark;
+
+public class GameTests
+{
+    [Fact]
+    public void ShouldReturnLoveAllForANewGame()
+    {
+        var game = new Game();
+        var result = game.GetStringScore();
+
+        Assert.Equal("love-all", result);
+    }
+
+    [Theory]
+    [InlineData(1, "15-love")]
+    [InlineData(2, "30-love")]
+    [InlineData(3, "40-love")]
+    [InlineData(4, "game-player-one")]
+    public void ShouldReturnLoveForANewGameAndOnePointsForPlayerOne(int points, string expectedScore)
+    {
+        var game = new Game();
+        for (var i = 0; i < points; i++)
+        {
+            game.AddPlayerOneScore();
+        }
+
+        var result = game.GetStringScore();
+
+        Assert.Equal(expectedScore, result);
+    }
+
+    [Theory]
+    [InlineData(1, "love-15")]
+    [InlineData(2, "love-30")]
+    [InlineData(3, "love-40")]
+    [InlineData(4, "game-player-two")]
+    public void ShouldReturnLoveForANewGameAndPointsForPlayerTwo(int points, string expectedScore)
+    {
+        var game = new Game();
+        for (var i = 0; i < points; i++)
+        {
+            game.AddPlayerTwoScore();
+        }
+
+        var result = game.GetStringScore();
+
+        Assert.Equal(expectedScore, result);
+    }
+
+    [Theory]
+    [InlineData(3, 3, "deuce")]
+    [InlineData(4, 3, "advantage-player-one")]
+    [InlineData(3, 4, "advantage-player-two")]
+    [InlineData(4, 4, "deuce")]
+    [InlineData(4, 5, "advantage-player-two")]
+    [InlineData(5, 4, "advantage-player-one")]
+    [InlineData(6, 4, "game-player-one")]
+    [InlineData(4, 6, "game-player-two")]
+    public void ShouldReturnExpectedScoreForBothPlayers(int pointsForA, int pointsForB, string expectedScore)
+    {
+        var game = new Game();
+        var iterations = Math.Max(pointsForA, pointsForB);
+        for (var i = 0; i < iterations; i++)
+        {
+            if (pointsForA > 0)
+            {
+                game.AddPlayerOneScore();
+                pointsForA--;
+            }
+
+            if (pointsForB > 0)
+            {
+                game.AddPlayerTwoScore();
+                pointsForB--;
+            }
+        }
+
+        var result = game.GetStringScore();
+
+        Assert.Equal(expectedScore, result);
+    }
+
+    [Theory]
+    [InlineData(5)]
+    public void ShouldReturnFalseWhenAddingPointsOverCompletedGameForPlayerOne(int points)
+    {
+        var game = new Game();
+        bool? lastResult = null;
+        for (var i = 0; i < points; i++)
+        {
+            lastResult = game.AddPlayerOneScore();
+        }
+
+        Assert.False(lastResult);
+    }
+    
+    [Theory]
+    [InlineData(5)]
+    public void ShouldReturnFalseWhenAddingPointsOverCompletedGameForPlayerTwo(int points)
+    {
+        var game = new Game();
+        bool? lastResult = null;
+        for (var i = 0; i < points; i++)
+        {
+            lastResult = game.AddPlayerTwoScore();
+        }
+
+        Assert.False(lastResult);
+    }
+}
