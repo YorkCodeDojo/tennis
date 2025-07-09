7f446f032c952a17e12953a6ad77b81ce5848b31
Gareth Lloyd <gareth.lloyd@memgraph.io>
2025-07-09 20:30:04 +0100

    wip

--- glloyd_and_dbetteridge/src/main.rs
+++ glloyd_and_dbetteridge/src/main.rs
@@ -1,51 +1,107 @@
+use std::fmt::Display;
+
 fn main() {
     println!("Hello, world!");
 }
 
-#[derive(PartialEq)]
-enum GameState{
-    eLoveAll,
-    e15Love,
-    e30Love,
-    eLove15
+#[derive(PartialEq, Copy, Clone)]
+enum PlayerScore {
+    Love,
+    Fifteen,
+    Thirty,
+    Forty,
+    Win,
 }
 
-struct Game {
-    state: GameState
+impl Display for PlayerScore {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Self::Love => "love",
+            Self::Fifteen => "15",
+            Self::Thirty => "30",
+            Self::Forty => "40",
+            Self::Win => "player1 has won",
+        };
+        write!(f, "{}", str)
+    }
 }
 
-impl Game {
-    pub(crate) fn point_for_player_two(&mut self) {
-        self.state = match self.state {
-            _ => GameState::eLove15,
+impl PlayerScore {
+    pub(crate) fn advance_score(self) -> Self {
+        match self {
+            Self::Love => Self::Fifteen,
+            Self::Fifteen => Self::Thirty,
+            Self::Thirty => Self::Forty,
+            Self::Forty => Self::Win,
+            Self::Win => Self::Win,
         }
     }
 }
 
-impl Game {
+#[derive(PartialEq, Copy, Clone)]
+enum Game {
+    Deuce,
+    AdvantagePlayer1,
+    AdvantagePlayer2,
+    Scores(PlayerScore, PlayerScore),
+}
 
+#[derive(PartialEq, Copy, Clone)]
+enum Player {
+    One,
+    Two,
+}
+
+impl Game {
     fn new() -> Game {
-        Game {
-            state: GameState::eLoveAll
+        Game::Scores(PlayerScore::Love, PlayerScore::Love)
+    }
+
+    pub(crate) fn advance_score(self, player: Player) -> Game {
+        match player {
+            Player::One => match self {
+                Game::Deuce => Game::AdvantagePlayer1,
+                Game::AdvantagePlayer1 => Game::AdvantagePlayer1,
+                Game::AdvantagePlayer2 => Game::AdvantagePlayer2,
+                Game::Scores(player1, player2) => match (player1, player2) {
+                    (PlayerScore::Win, _) => Game::Scores(player1, player2),
+                    (_, PlayerScore::Win) => Game::Scores(player1, player2),
+                    (PlayerScore::Thirty, PlayerScore::Forty) => Game::Deuce,
+                    _ => Game::Scores(player1.advance_score(), player2),
+                },
+            },
+            Player::Two => match self {
+                Game::Deuce => Game::AdvantagePlayer2,
+                Game::AdvantagePlayer1 => Game::AdvantagePlayer1,
+                Game::AdvantagePlayer2 => Game::AdvantagePlayer2,
+                Game::Scores(player1, player2) => match (player1, player2) {
+                    (PlayerScore::Win, _) => Game::Scores(player1, player2),
+                    (_, PlayerScore::Win) => Game::Scores(player1, player2),
+                    (PlayerScore::Forty, PlayerScore::Thirty) => Game::Deuce,
+                    _ => Game::Scores(player1, player2.advance_score()),
+                },
+            },
         }
     }
+
     pub(crate) fn point_for_player_one(&mut self) {
-        if self.state == GameState::eLoveAll
-        {
-            self.state = GameState::e15Love;
-        }
-        else {
-            self.state = GameState::e30Love;
-        }
+        *self = self.advance_score(Player::One);
+    }
+
+    pub(crate) fn point_for_player_two(&mut self) {
+        *self = self.advance_score(Player::Two);
     }
 
     pub(crate) fn print_score(&self) -> String {
-         match self.state {
-             GameState::eLoveAll => "love-all",
-             GameState::e15Love => "15-love",
-             GameState::e30Love => "30-love",
-             GameState::eLove15 => "love-15",
-         }.into()
+        match self {
+            Game::Deuce => format!("Deuce"),
+            Game::AdvantagePlayer1 => format!("Advantage player1"),
+            Game::AdvantagePlayer2 => format!("Advantage player2"),
+            Game::Scores(PlayerScore::Win, _) => format!("player1 has won"),
+            Game::Scores(_, PlayerScore::Win) => format!("player2 has won"),
+            Game::Scores(lhs, rhs) if lhs == rhs => format!("{}-all", lhs),
+            Game::Scores(lhs, rhs) => format!("{}-{}", lhs, rhs),
+        }.into()
     }
 }
 
@@ -58,7 +114,6 @@ mod tests {
         let sut = Game::new();
         assert_eq!(sut.print_score(), "love-all");
     }
-
 
     #[test]
     fn if_player1_once_then_has_score_15_love() {
@@ -80,5 +135,54 @@ mod tests {
         let mut sut = Game::new();
         sut.point_for_player_two();
         assert_eq!(sut.print_score(), "love-15");
+    }
+
+    #[test]
+    fn if_player1_scores_three_times_then_has_score_40_love() {
+        let mut sut = Game::new();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        assert_eq!(sut.print_score(), "40-love");
+    }
+
+    #[test]
+    fn if_player1_scores_four_times_then_has_player1_has_won() {
+        let mut sut = Game::new();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        assert_eq!(sut.print_score(), "player1 has won");
+    }
+
+    #[test]
+    fn if_both_players_have_40_then_its_deuce() {
+        let mut sut = Game::new();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+
+        sut.point_for_player_two();
+        sut.point_for_player_two();
+        sut.point_for_player_two();
+
+        assert_eq!(sut.print_score(), "Deuce");
+    }
+
+    #[test]
+    fn from_deuce_player1_can_take_the_advantage() {
+        let mut sut = Game::new();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+        sut.point_for_player_one();
+
+        sut.point_for_player_two();
+        sut.point_for_player_two();
+        sut.point_for_player_two();
+
+        sut.point_for_player_one();
+
+        assert_eq!(sut.print_score(), "Advantage player1");
     }
 }
